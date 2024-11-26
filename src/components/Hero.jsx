1c35ac508336e6e3d545550a9430b1847f35732f
Abdelmalek Maskri <abdelmalekmaskri18@gmail.com>
2024-11-26 17:16:03 +0000

    fixed some styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,10 @@ export default function Hero() {
                 <p>IT'S TIME TO GET</p>
                 <h1 className='uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'><span className='text-blue-400'>BIIIG</span></h1>
             </div>
-            <p className='text-sm md:text-base font-light'>I hereby acknowledgement that I may become <span className='text-blue-400 font-medium'>unbelievably swolenormous</span> and accept all risks of becoming the local <span className='text-blue-400 font-medium'>mass montrosity</span>, afflicted with severe body dismorphia, unable to fit through doors.</p>
+            <p className='text-base md:text-lg font-normal leading-relaxed'>
+            By proceeding, I fully embrace the possibility of becoming <span className='text-purple-500 font-bold'>ridiculously shredded</span>, taking on the esteemed title of the local <span className='text-purple-500 font-bold'>muscle monarch</span>. I accept all challenges, including potential struggles with narrow doorways and an overabundance of mirror selfies.
+            </p>
+
             <Button func={() => {
                 window.location.href = '#generate'
             }} text={"Accept & Begin"}></Button>
